02d7937b9228d009fd0e916cf7cc35cfe92ee8cd
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-09-27 10:25:19 -0500

    Changed url of api requests. Removed console.log from modal. Removed app.js.

--- src/app.js
+++
@@ -1,6 +0,0 @@
-const express     = require("express"),
-    app         = express();
-
-    app.listen(4000, process.env.IP, () =>{
-        console.log("What's Cooking Server Has Started!");
-     });
